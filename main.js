846c2e30a0e34ca8d60bdc2d9b5558e94067ab98
ashwini <ashwini.shetty@crestit.in>
2020-11-21 23:21:22 +0530

    V1.0.12

--- main.js
+++ main.js
@@ -1,7 +1,58 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const electron = require('electron');
+const remote = require('electron').remote;
+const url = require('url');
+const path = require('path');
+const { dialog } = require('electron');
+const os = require('os');
+const si = require('systeminformation');
+const mysql = require('mysql');
+const ip = require('ip');
+const { session } = require('electron');
+const osu = require('node-os-utils');
+const request = require("request");
+const cron = require('node-cron'); 
+const fs = require("fs");
+const log = require("electron-log");
+const exec = require('child_process').exec;
+const AutoLaunch = require('auto-launch');
+const nodeDiskInfo = require('node-disk-info');
+const mv = require('mv');
+const uuid = require('node-machine-id');
+const psList = require('ps-list');
+const csv = require('csvtojson');
+const serialNumber = require('serial-number');
 const { autoUpdater } = require('electron-updater');
 
+const Tray = electron.Tray;
+const iconPath = path.join(__dirname,'images/ePrompto_png.png');
+
+global.root_url = 'http://localhost/end_user_backend';
+
+const {app, BrowserWindow, screen, ipcMain} = electron;
+let reqPath = path.join(app.getAppPath(), '../');
+const detail =  reqPath+"syskey.txt";
+var csvFilename = reqPath + 'utilise.csv';
+var time_file = reqPath + 'time_file.txt';
+
 let mainWindow;
+let categoryWindow;
+let settingWindow;
+let display;
+let width;
+let startWindow;
+let tabWindow;
+let child;
+let ticketIssue;
+
+let tray = null;
+let count = 0;
+var crontime_array = [];
+
+let loginWindow;
+let regWindow;
+let forgotWindow;
+let ticketWindow;
+let quickUtilWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({
