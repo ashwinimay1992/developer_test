25569ed07007716c0ccbf4d91e3bafe37096caf0
ashwini <ashwini.shetty@crestit.in>
2020-11-24 13:48:47 +0530

    v 1.0.22

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ const exec = require('child_process').exec;
 const AutoLaunch = require('auto-launch');
 const nodeDiskInfo = require('node-disk-info');
 const mv = require('mv');
-// const uuid = require('node-machine-id');
-// const psList = require('ps-list');
+const uuid = require('node-machine-id');
+const psList = require('ps-list');
 // const csv = require('csvtojson');
 // const serialNumber = require('serial-number');
 
